c88bd22a4a3d8d3a235163f6af9d08ce4a45b112
BiteSizeProblems <97358081+BiteSizeProblems@users.noreply.github.com>
2024-02-15 21:34:46 -0500

    Add project files.

---
+++ Slipstream-Telemetry-Exporter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Slipstream_Telemetry_Exporter;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
